93e841d4faa5be6592ffc73e1e6357cf5081d3c8
Wendy96-wj <wendyonezw@163.com>
2019-11-07 17:59:03 +0800

    1107

--- vue_cli3/my_tabbar/src/App.vue
+++ vue_cli3/my_tabbar/src/App.vue
@@ -3,22 +3,22 @@
     <tab-bar>
       <TabBarItem>
         <img slot="item.icon" src="./assets/images/tabbar/indexno.png" alt="">
-        <img slot="item.icon" src="./assets/images/tabbar/indexsel.png" alt="">
+        <img slot="item.icon-active" src="./assets/images/tabbar/indexsel.png" alt="">
         <div slot="item.text">首页</div>
       </TabBarItem>
       <TabBarItem>
         <img slot="item.icon" src="./assets/images/tabbar/fbsel.png" alt="">
-        <img slot="item.icon" src="./assets/images/tabbar/fbsel.png" alt="">
+        <img slot="item.icon-active" src="./assets/images/tabbar/fbsel.png" alt="">
         <div slot="item.text">分类</div>
       </TabBarItem>
       <TabBarItem>
         <img slot="item.icon" src="./assets/images/tabbar/scno.png" alt="">
-        <img slot="item.icon" src="./assets/images/tabbar/scsel.png" alt="">
+        <img slot="item.icon-active" src="./assets/images/tabbar/scsel.png" alt="">
         <div slot="item.text">购物车</div>
       </TabBarItem>
       <TabBarItem>
         <img slot="item.icon" src="./assets/images/tabbar/myno.png" alt="">
-        <img slot="item.icon" src="./assets/images/tabbar/mysel.png" alt="">
+        <img slot="item.icon-active" src="./assets/images/tabbar/mysel.png" alt="">
         <div slot="item.text">我的</div>
       </TabBarItem>
     </tab-bar>
--- vue_cli3/my_tabbar/src/components/tabBar/TabBarItem.vue
+++ vue_cli3/my_tabbar/src/components/tabBar/TabBarItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tab-bar-item">
-    <div v-if="!isActive"><slot name="item.icon"></slot></div>
+    <div v-if="isActive"><slot name="item.icon"></slot></div>
     <div v-else><slot name="item.icon-active"></slot></div>
     <div :class="{active:isActive}"><slot name="item.text"></slot></div>   
   </div>
